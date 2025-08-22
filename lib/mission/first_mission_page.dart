0ff8cf47d62aecbe053e3a7482750357f4259f71
99minj0731 <99minj0731@naver.com>
2025-08-22 23:17:44 +0900

    made_widget

--- lib/mission/first_mission_page.dart
+++ lib/mission/first_mission_page.dart
@@ -3,6 +3,19 @@ import 'package:flutter/material.dart';
 class FirstMissionPage extends StatelessWidget {
   const FirstMissionPage({super.key});
 
+  final String buttonTitle = "유튜브 쇼핑으로 이동";
+
+  final String imgSrc =
+      "https://plus.unsplash.com/premium_photo-1680994646651-bb647d9ff008?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dmFjYW5jZXN8ZW58MHx8MHx8fDA%3D";
+  final String title = "제목";
+  final String desc = "설명입니다.";
+
+  static final Widget missionTitle = Text(
+    '최신 제휴사 혜택',
+    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+  );
+  static final Widget missionBox = Container(height: 100, color: Colors.yellow);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +30,36 @@ class FirstMissionPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               spacing: 10,
               children: [
-                _getTitle(),
-                _getBanner(),
-                _getBanner(),
-                _getButton(title: "유튜브 쇼핑으로 이동"),
+                missionTitle,
+                Row(
+                  spacing: 10,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        height: 100,
+                        width: 100,
+                        imgSrc,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text(title), Text(desc)],
+                    ),
+                  ],
+                ),
+                BannerItem(imgSrc: imgSrc, title: title, desc: desc),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.grey,
+                  ),
+                  onPressed: () {
+                    print('버튼 클릭됨 : $title');
+                  },
+                  child: Text(title),
+                ),
               ],
             ),
           ),
@@ -30,54 +69,89 @@ class FirstMissionPage extends StatelessWidget {
   }
 }
 
-Widget _getTitle() {
-  return Text(
-    '최신 제휴사 혜택',
-    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-  );
-}
+class BannerItem extends StatelessWidget {
+  const BannerItem({
+    super.key,
+    required this.imgSrc,
+    required this.title,
+    required this.desc,
+  });
 
-Widget _getBox({double height = 50}) {
-  return Container(height: 100, color: Colors.yellow);
-}
+  final String imgSrc;
+  final String title;
+  final String desc;
 
-Widget _getButton({required String title}) {
-  var btnStyle = TextButton.styleFrom(
-    foregroundColor: Colors.white,
-    backgroundColor: Colors.grey,
-  );
-
-  return TextButton(
-    style: btnStyle,
-    onPressed: () {
-      print('버튼 클릭됨 : $title');
-    },
-    child: Text(title),
-  );
-}
-
-Widget _getBanner({
-  String imgSrc =
-      "https://plus.unsplash.com/premium_photo-1680994646651-bb647d9ff008?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dmFjYW5jZXN8ZW58MHx8MHx8fDA%3D",
-  String title = "제목",
-  String desc = "설명입니다.",
-}) {
-  return Row(
-    spacing: 10,
-    children: [
-      ClipRRect(
-        borderRadius: BorderRadius.circular(8.0),
-        child: Image.network(
-          height: 100,
-          width: 100,
-          imgSrc,
-          fit: BoxFit.cover,
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 10,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Image.network(
+            height: 100,
+            width: 100,
+            imgSrc,
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [Text(title), Text(desc)],
-      ),
-    ],
-  );
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [Text(title), Text(desc)],
+        ),
+      ],
+    );
+  }
 }
+
+// Widget _getTitle() {
+//   return Text(
+//     '최신 제휴사 혜택',
+//     style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//   );
+// }
+
+// Widget _getBox({double height = 50}) {
+//   return Container(height: 100, color: Colors.yellow);
+// }
+
+// Widget _getButton({required String title}) {
+//   var btnStyle = TextButton.styleFrom(
+//     foregroundColor: Colors.white,
+//     backgroundColor: Colors.grey,
+//   );
+
+//   return TextButton(
+//     style: btnStyle,
+//     onPressed: () {
+//       print('버튼 클릭됨 : $title');
+//     },
+//     child: Text(title),
+//   );
+// }
+
+// Widget _getBanner({
+//   String imgSrc =
+//       "https://plus.unsplash.com/premium_photo-1680994646651-bb647d9ff008?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dmFjYW5jZXN8ZW58MHx8MHx8fDA%3D",
+//   String title = "제목",
+//   String desc = "설명입니다.",
+// }) {
+//   return Row(
+//     spacing: 10,
+//     children: [
+//       ClipRRect(
+//         borderRadius: BorderRadius.circular(8.0),
+//         child: Image.network(
+//           height: 100,
+//           width: 100,
+//           imgSrc,
+//           fit: BoxFit.cover,
+//         ),
+//       ),
+//       Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [Text(title), Text(desc)],
+//       ),
+//     ],
+//   );
+// }
