9c340c0187b0e734d50df149f61beb231c5d788a
99minj0731 <99minj0731@naver.com>
2025-08-24 23:06:28 +0900

    youtube_short_section

--- lib/youtube/youtube_home_page.dart
+++ lib/youtube/youtube_home_page.dart
@@ -4,6 +4,8 @@ class YoutubeHomePage extends StatelessWidget {
   YoutubeHomePage({super.key});
 
   final focusNode = FocusNode();
+  final String imgUrl =
+      'https://plus.unsplash.com/premium_photo-1677181729163-33e6b59d5c8f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
   @override
   Widget build(BuildContext context) {
@@ -50,13 +52,58 @@ class YoutubeHomePage extends StatelessWidget {
           scrollDirection: Axis.vertical,
           children: [
             SizedBox(
-              height: 150,
+              height: 250,
               child: ListView(
                 scrollDirection: Axis.horizontal,
                 children: List.generate(100, (index) {
                   return Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Container(width: 80, color: Colors.deepOrange),
+                    child: SizedBox(
+                      width: 150,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Image.network(imgUrl, fit: BoxFit.cover),
+                            Container(color: Colors.black.withAlpha(100)),
+                            Positioned(
+                              top: 0,
+                              right: 0,
+                              child: IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.more_vert,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "사료 못먹는 고양이의 밥",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    Text(
+                                      "조회수 162만회",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                   );
                 }),
               ),
@@ -64,7 +111,7 @@ class YoutubeHomePage extends StatelessWidget {
             ...List.generate(100, (index) {
               return Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Container(height: 150, color: Colors.deepOrange),
+                child: Container(height: 300, color: Colors.deepOrange),
               );
             }),
           ],
